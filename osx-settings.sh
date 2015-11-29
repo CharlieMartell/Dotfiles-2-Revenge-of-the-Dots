d5ae1d99f430d3f55f1629b62333f7e687e605cf
CharlieMartell <charliecmartell@gmail.com>
2015-11-29 17:58:59 -0600

    Trackpad info added to osx-settings

--- osx-settings.sh
+++ osx-settings.sh
@@ -15,6 +15,36 @@ defaults write com.apple.dock autohide-time-modifier -float 0;
 # show all file extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true; 
 
+# Setup Trackpad information
+defaults write AppleMultitouchTrackpad ActuateDetents -int 1;
+defaults write AppleMultitouchTrackpad Clicking -int 1;
+defaults write AppleMultitouchTrackpad DragLock -int 0;
+defaults write AppleMultitouchTrackpad Dragging -int 0;
+defaults write AppleMultitouchTrackpad FirstClickThreshold -int 1;
+defaults write AppleMultitouchTrackpad ForceSuppressed -int 0;
+defaults write AppleMultitouchTrackpad SecondClickThreshold -int 1;
+defaults write AppleMultitouchTrackpad TrackpadCornerSecondaryClick -int 1;
+defaults write AppleMultitouchTrackpad TrackpadFiveFingerPinchGesture -int 0;
+defaults write AppleMultitouchTrackpad TrackpadFourFingerHorizSwipeGesture -int 2;
+defaults write AppleMultitouchTrackpad TrackpadFourFingerPinchGesture -int 0;
+defaults write AppleMultitouchTrackpad TrackpadFourFingerVertSwipeGesture -int 2;
+defaults write AppleMultitouchTrackpad TrackpadHandResting -int 1;
+defaults write AppleMultitouchTrackpad TrackpadHorizScroll -int 1;
+defaults write AppleMultitouchTrackpad TrackpadMomemtumScroll -int 1;
+defaults write AppleMultitouchTrackpad TrackpadPinch -int 1;
+defaults write AppleMultitouchTrackpad TrackpadRightClick -int 1;
+defaults write AppleMultitouchTrackpad TrackpadRotate -int 1;
+defaults write AppleMultitouchTrackpad TrackpadScroll -int 1;
+defaults write AppleMultitouchTrackpad TrackpadThreeFingerDrag -int 0;
+defaults write AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture -int 2;
+defaults write AppleMultitouchTrackpad TrackpadThreeFingerTapGesture -int 2;
+defaults write AppleMultitouchTrackpad TrackpadThreeFingerVertSwipeGesture -int 2;
+defaults write AppleMultitouchTrackpad TrackpadTwoFingerDoubleTapGesture -int 2;
+defaults write AppleMultitouchTrackpad TrackpadTwoFingerFromRightEdgeSwipeGesture -int 0;
+defaults write AppleMultitouchTrackpad USBMouseStopsTrackpad -int 0;
+defaults write AppleMultitouchTrackpad UserPreferences -int 1;
+defaults write AppleMultitouchTrackpad version -int 10;
+
 # Reset Finder 
-killall Dock 2>/dev/null;
-killall Finder 2>/dev/null;
+killall Dock 2> /dev/null;
+killall Finder 2> /dev/null;
