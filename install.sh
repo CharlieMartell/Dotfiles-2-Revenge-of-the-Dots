75f3894e8afe0be110f99a33aeb9398c3353d57d
CharlieMartell <charliecmartell@gmail.com>
2015-12-01 13:13:54 -0600

    Installation script baseline

--- install.sh
+++ install.sh
@@ -6,3 +6,19 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `osx-settings` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Installing system configuration for OSX"
+
+    echo "Installing command line tools"
+    # Hack to check if gcc is installed, if not installed cli tools
+    which gcc | /bin/sh ./command-line-tools.sh
+
+    echo "Installing Applications from Homebrew"
+    /bin/sh ./brews.sh
+
+    echo "Setting OSX Defaults"
+    /bin/sh ./osx-settings.sh
+fi
+
+# TODO: Handle ZSH/VIM etc
+
